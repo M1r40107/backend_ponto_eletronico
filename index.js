67ccc95e29b84951273e62e46ebc519472ed43e7
Guilherme Miranda <guilherme.mirandac@sempreceub.com>
2024-10-29 10:44:10 -0300

    atualizacoes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+let express = require('express');
+let app = express();
+
+app.get('/abc', (req, res) => {
+    res.send("Chamada ao recurso realizada com sucesso");
+});
+
+//retornar todos os usuários
+app.get('/users', (req, res) => {
+    res.send("Usuários do sistema");
+});
+
+app.get('/user:id', (req, res) => {
+    console.log(req);
+});
+
+app.listen(3000, () => {
+    console.log("Servidor aguardando requisições");
+});
+
+app.get('/', (req, res) => {
+    res.send("Chamada ao recurso get realizada");
+});
+
+app.post('/rotapost', (req, res) => {
+    res.send("Chamada ao recurso post realizada");
+});
